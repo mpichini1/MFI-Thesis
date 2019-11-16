212122f0da949f8f4bc7dd616906ff3558277d0f
mpichini1 <marcello.pichini@gmail.com>
2019-11-16 18:14:23 +0100

    Code for pdf reading

---
+++ read_pdf.py
@@ -0,0 +1,28 @@
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import PDFPageAggregator
+import io 
+
+from pdfminer.converter import TextConverter
+resource_manager = PDFResourceManager()
+fake_file_handle = io.StringIO()
+converter = TextConverter(resource_manager, fake_file_handle)
+page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+with open('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Balanced/LU0346389934.pdf', 'rb') as fh:
+
+    for page in PDFPage.get_pages(fh,
+                                  caching=True,
+                                  check_extractable=True):
+        page_interpreter.process_page(page)
+
+    text = fake_file_handle.getvalue()
+
+# close open handles
+converter.close()
+fake_file_handle.close()
+
+print(text)
+
