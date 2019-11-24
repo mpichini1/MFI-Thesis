92440f2e724db74e5b3d378432d3f7a0d350d570
mpichini1 <marcello.pichini@gmail.com>
2019-11-24 19:09:33 +0100

    update

--- get_title.py
+++ get_title.py
@@ -1,16 +1,16 @@
 import PyPDF2
 from PyPDF2 import PdfFileReader
+from pdfrw import PdfReader
 
-
-input = PyPDF2.PdfFileReader('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Balanced/LU0622328622.pdf')
+input = PyPDF2.PdfFileReader('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Bond/LU0441497293.pdf')
 if input.isEncrypted:
     input.decrypt('')
 
 def get_pdf_title(pdf_file_path):
 
-    
-    return input.getDocumentInfo().title
+    return PdfReader().Info.Title
+    #return input.getDocumentInfo().title
 
-title = get_pdf_title('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Balanced/LU0622328622.pdf')
+title = get_pdf_title('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Bond/LU0441497293.pdf')
 
 print(title)
--- read_pdf.py
+++ read_pdf.py
@@ -4,25 +4,33 @@ from pdfminer3.pdfinterp import PDFResourceManager
 from pdfminer3.pdfinterp import PDFPageInterpreter
 from pdfminer3.converter import PDFPageAggregator
 import io 
-
+import os
 from pdfminer.converter import TextConverter
 resource_manager = PDFResourceManager()
 fake_file_handle = io.StringIO()
 converter = TextConverter(resource_manager, fake_file_handle)
 page_interpreter = PDFPageInterpreter(resource_manager, converter)
 
-with open('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Balanced/LU0346389934.pdf', 'rb') as fh:
 
-    for page in PDFPage.get_pages(fh,
-                                  caching=True,
-                                  check_extractable=True):
-        page_interpreter.process_page(page)
+def pdfreading():
 
-    text = fake_file_handle.getvalue()
+		
+	with open('C:/Users/mpich/Desktop/MFI-Thesis/KIID/Balanced/LU1006080730.pdf', 'rb') as fh:
 
-# close open handles
-converter.close()
-fake_file_handle.close()
+		for page in PDFPage.get_pages(fh,
+			caching=True,
+			check_extractable=True):
+			page_interpreter.process_page(page)
 
-print(text)
+			text = fake_file_handle.getvalue()
+
+	# close open handles
+	converter.close()
+	fake_file_handle.close()
+
+	
+	
+	return text
+
+#print(pdfreading())
 
